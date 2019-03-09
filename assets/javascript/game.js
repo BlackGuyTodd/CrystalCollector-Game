1a3e9c2a0a3370910839e7de0036ff3459018b6e
Todd Tate <toddtatevw@gmail.com>
2019-03-08 17:29:59 -0700

    Finished functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,9 @@
 var winsCounter = 0;
 var lossesCounter = 0;
 var counter = 0;
-var targetScore = Math.floor(Math.random() * 100) + 1;
+var targetScore;
 var numberOptions = [5, 10, 11, 12];
+var imgOptions = ["assets/images/blue_gem.jpg", "assets/images/green_gem.jpg", "assets/images/pink_gem.jpg", "assets/images/yellow_gem.jpg"];
 var increment = numberOptions[Math.round(Math.random())];
 var crystalValue = ($(this).attr("data-crystalvalue"));
 
@@ -12,66 +13,97 @@ var crystalValue = ($(this).attr("data-crystalvalue"));
 
 //Remove images from html because they will be dynamically created below.
 //Change image options to only a couple numbers probably just 4
-window.onload = function () {
-    for (var i = 0; i < numberOptions.length; i++) {
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "assets/images/blue_gem.jpg");
-        imageCrystal.attr("data-crystal-value", numberOptions[i]);
-        $(".testing").append(imageCrystal);
+$(document).ready(function () {
+    function createButtons() {
+        $(".testing").html("");
+        targetScore = Math.floor(Math.random() * 100) + 20;
+        $("#score-to-beat").text(targetScore);
+        counter = 0;
+        $("#player-score").text(counter);
+        for (var i = 0; i < imgOptions.length; i++) {
+            console.log(imgOptions[i])
+            var imageCrystal = $("<img>");
+            imageCrystal.addClass("crystal");
+            imageCrystal.attr("src", imgOptions[i]);
+            imageCrystal.attr("data-crystal-value", (Math.floor(Math.random() * 12) + 1));
+            $(".testing").append(imageCrystal);
 
-        imageCrystal.attr("src", "assets/images/green_gem.jpg");
-        imageCrystal.attr("data-crystal-value", numberOptions[i]);
-        $(".testing").append(imageCrystal);
-    };
+        };
 
-    $("#score-to-beat").text(targetScore);
+        $("img.crystal").on("click", function () {
+            console.log($(this).attr("data-crystal-value"));
+            checkScore($(this).attr("data-crystal-value"));
+        });
+    }
+
+    
     //CLICK EVENTS========================================================
 
     //When the gem images are clicked on this is what happens.
-    $("#blue-gem").on("click", function () {
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
+    function checkScore(value) {
+        counter += parseInt(value);
+        $("#player-score").text(counter);
+
         if (counter === targetScore) {
             alert("You Win!");
+            winsCounter++
+            $("#wins").text(winsCounter);
+            createButtons();
         }
-        else if (counter >= targetScore) {
+        else if (counter > targetScore) {
             alert("You Lose");
+            lossesCounter++
+            $("#losses").text(lossesCounter);
+            createButtons();
         }
-    });
+    }
 
-    $("#green-gem").on("click", function () {
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        if (counter === targetScore) {
-            alert("You Win!");
-        }
-        else if (counter >= targetScore) {
-            alert("You Lose");
-        }
-    });
 
-    $("#pink-gem").on("click", function () {
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        if (counter === targetScore) {
-            alert("You Win!");
-        }
-        else if (counter >= targetScore) {
-            alert("You Lose");
-        }
-    });
+    createButtons();
 
-    $("#yellow-gem").on("click", function () {
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        if (counter === targetScore) {
-            alert("You Win!");
-        }
-        else if (counter >= targetScore) {
-            alert("You Lose");
-        }
-    });
+    // $("#blue-gem").on("click", function () {
+    //     crystalValue = parseInt(crystalValue);
+    //     counter += crystalValue;
+    //     if (counter === targetScore) {
+    //         alert("You Win!");
+    //     }
+    //     else if (counter >= targetScore) {
+    //         alert("You Lose");
+    //     }
+    // });
 
-};
+    // $("#green-gem").on("click", function () {
+    //     crystalValue = parseInt(crystalValue);
+    //     counter += crystalValue;
+    //     if (counter === targetScore) {
+    //         alert("You Win!");
+    //     }
+    //     else if (counter >= targetScore) {
+    //         alert("You Lose");
+    //     }
+    // });
+
+    // $("#pink-gem").on("click", function () {
+    //     crystalValue = parseInt(crystalValue);
+    //     counter += crystalValue;
+    //     if (counter === targetScore) {
+    //         alert("You Win!");
+    //     }
+    //     else if (counter >= targetScore) {
+    //         alert("You Lose");
+    //     }
+    // });
+
+    // $("#yellow-gem").on("click", function () {
+    //     crystalValue = parseInt(crystalValue);
+    //     counter += crystalValue;
+    //     if (counter === targetScore) {
+    //         alert("You Win!");
+    //     }
+    //     else if (counter >= targetScore) {
+    //         alert("You Lose");
+    //     }
+    // });
+
+});
 
